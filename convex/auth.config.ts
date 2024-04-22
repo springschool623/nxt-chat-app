ab48cf4e1c66c2f5a7eacb5ed7f212a92cd30455
Your Name <you@example.com>
2024-04-22 14:54:50 +0700

    Webhooks Added

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: 'https://present-pony-96.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+}
+
+export default authConfig
