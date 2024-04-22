8c1f2f5fc8dded2a5de7f7588a6cbe946dd5a1d5
Your Name <you@example.com>
2024-04-22 16:55:37 +0700

    getMyConversation query added

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -6,10 +6,15 @@ import { conversations } from '@/dummy-data/db'
 import Conversation from './conversation'
 import { UserButton } from '@clerk/nextjs'
 import UserListDialog from './user-list-dialog'
-import { useConvexAuth } from 'convex/react'
+import { useConvexAuth, useQuery } from 'convex/react'
+import { api } from '../../../convex/_generated/api'
 
 const LeftPanel = () => {
   const { isAuthenticated } = useConvexAuth()
+  const conversations = useQuery(
+    api.conversations.getMyConversations,
+    isAuthenticated ? undefined : 'skip'
+  )
   return (
     <div className="w-1/4 border-gray-600 border-r">
       <div className="sticky top-0 bg-left-panel z-10">
@@ -42,7 +47,7 @@ const LeftPanel = () => {
       {/* Chat List */}
       <div className="my-3 flex flex-col gap-0 max-h-[80%] overflow-auto">
         {/* Conversations will go here*/}
-        {conversations.map((conversation) => (
+        {conversations?.map((conversation) => (
           <Conversation key={conversation._id} conversation={conversation} />
         ))}
 
