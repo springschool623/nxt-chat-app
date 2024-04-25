975c66e3d49c82ef776e02bbff6d22f067eae986
Your Name <you@example.com>
2024-04-25 13:16:15 +0700

    KickUser Mutation Added

--- src/components/home/chat-bubble.tsx
+++ src/components/home/chat-bubble.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { Dialog, DialogContent, DialogDescription } from '../ui/dialog'
 import ReactPlayer from 'react-player'
+import ChatAvatarActions from './chat-avatar-actions'
 
 type ChatBubbleProps = {
   message: IMessage
@@ -59,6 +60,7 @@ const ChatBubble = ({ me, message, previousMessage }: ChatBubbleProps) => {
             className={`flex flex-col z-20 max-w-fit px-2 pt-1 rounded-md shadow-md relative ${bgClass}`}
           >
             <OtherMessageIndicator />
+            {isGroup && <ChatAvatarActions message={message} me={me} />}
             {renderMessageContent()}
             {open && (
               <ImageDialog
